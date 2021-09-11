0b48c4e4c21d8a255f6f2084ff04890244572bac
Josscoder <josscoder@hotmail.com>
2021-09-11 10:11:41 -0500

    feat: block bug fixed

--- src/jossc/volcano/entity/CustomFallingWoolBlock.php
+++ src/jossc/volcano/entity/CustomFallingWoolBlock.php
@@ -3,13 +3,16 @@
 namespace jossc\volcano\entity;
 
 use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
 use pocketmine\entity\object\FallingBlock;
 
 class CustomFallingWoolBlock extends FallingBlock {
 
-    protected function initEntity(): void
-    {
-        $this->block = Block::get(Block::WOOL, rand(0, 15));
+    protected function initEntity(): void {
+        $this->block = BlockFactory::get(Block::WOOL, rand(0, 15));
+
+        $this->propertyManager->setInt(self::DATA_VARIANT, $this->block->getRuntimeId());
+
         $this->setForceMovementUpdate(true);
         $this->setCanSaveWithChunk(false);
     }
@@ -18,8 +21,7 @@ class CustomFallingWoolBlock extends FallingBlock {
      * @param int $tickDiff
      * @return bool
      */
-    public function entityBaseTick(int $tickDiff = 1): bool
-    {
+    public function entityBaseTick(int $tickDiff = 1): bool {
         if ($this->isClosed()) {
             return false;
         }
